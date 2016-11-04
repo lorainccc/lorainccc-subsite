18005c4fe4b113882412891a4658c925f29f9bec
Joe Querin <joecue@gmail.com>
2016-11-04 13:47:20 -0400

    Header Shortcut Mobile Menu - New Program Path Template

--- template-parts/content-breadcrumb.php
+++ template-parts/content-breadcrumb.php
@@ -1,1 +1,24 @@
--
+<?php
+
+   //LCCC Custom Breadcrumb Display Code
+  if (function_exists('lccc_breadcrumb')){
+    if (is_home() || is_front_page()) {
+     echo lccc_breadcrumb() . get_bloginfo('name') ;
+    } elseif ( is_category() ) {
+    	echo lccc_breadcrumb() . "<a href='" . get_bloginfo('url') . "'>" . get_bloginfo('name') . "</a> > " . single_cat_title() ;
+    }elseif ( is_archive() ) {
+	echo lccc_breadcrumb() . "<a href='" . get_bloginfo('url') . "'>" . get_bloginfo('name') . "</a> > " . single_cat_title( '', false ) ;
+    } elseif ( is_tax() ) {
+        echo lccc_breadcrumb() . "<a href='" . get_bloginfo('url') . "'>" . get_bloginfo('name') . "</a> > " . single_cat_title( '', false ) ;
+    }elseif ( is_page() && $post->post_parent != 0 ) {
+     // Page is a Subpage
+     echo lccc_breadcrumb() . "<a href='" . get_bloginfo('url') . "'>" . get_bloginfo('name') . "</a> > " . "<a href='" . get_permalink( $post->post_parent ) ."'>" . get_the_title( $post->post_parent ) . "</a>" . " > " . get_the_title() ;
+    }elseif ( is_page() && $post->post_parent > 0 ) {
+ echo lccc_breadcrumb() . "<a href='" . get_bloginfo('url') . "'>" . get_bloginfo('name') . "</a> > ";
+     }elseif ( is_single() ) {
+	echo lccc_breadcrumb() . "<a href='" . get_bloginfo('url') . "'>" . get_bloginfo('name') . "</a> > " . get_the_term_list( $post->ID, 'event_categories','', ' , ' , '') . " > " . get_the_title()  ;
+    }else {
+    	echo lccc_breadcrumb() . "<a href='" . get_bloginfo('url') . "'>" . get_bloginfo('name') . "</a> > " . get_the_title() ;
+    };
+   };
+  ?>
