070f8b1e4aba1c45b04772ccd45c266b23264f17
Joe Querin <7998323+joecue@users.noreply.github.com>
2017-11-17 16:21:35 -0500

    Adding a Row

--- emailer.php
+++ emailer.php
@@ -32,9 +32,11 @@
 	
 		<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-					<div class="small-12 medium-8 columns gateway-header hide-for-print">
+			<div class="row">
+			<div class="small-12 medium-8 columns gateway-header hide-for-print">
 					<?php the_post_thumbnail(); ?>
 					</div>
+			</div>
 			<div class="row">
 				<div class="small-12 medium-8 columns">
 					<?php while ( have_posts() ) : the_post();
