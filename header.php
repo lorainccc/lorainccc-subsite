b1f2168e4531974e8650e39857ca7a37bded4b30
lmartinlccc <35542579+lmartinlccc@users.noreply.github.com>
2018-06-14 09:23:21 -0400

    Add Google Tag Manager Code

--- header.php
+++ header.php
@@ -26,6 +26,14 @@
   ga('send', 'pageview');
 
 </script>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PWJL5TQ');</script>
+<!-- End Google Tag Manager -->
 	
 <!-- Hotjar Tracking Code for www.lorainccc.edu 2-->
 <script>
@@ -60,6 +68,12 @@
 
 </head>
 <body <?php body_class(); ?>>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWJL5TQ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text hide-for-print" href="#content"><?php esc_html_e( 'Skip to content', 'lccc-framework' ); ?></a>
 
