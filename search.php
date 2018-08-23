83e532ae2b66a1ec11a580be3d3d699b374718c4
Joe Querin <7998323+joecue@users.noreply.github.com>
2018-08-23 10:28:48 -0400

    :wrench: Removed query result count

--- search.php
+++ search.php
@@ -9,13 +9,6 @@ get_header();
 </div>
 <div class="row">
 	<div class="small-12 columns">
-		<?php 
-		
-		global $wp_query;
-		$count = $wp_query->post_count; 
-		
-		?>
-		<h2>Results returned: <?php echo $count; ?></h2>
 <?php	
 
  while ( have_posts() ) : the_post();
