81b4a357533119244a41b26ae5ebf41beec06a03
Joe <joecue@gmail.com>
2019-04-30 10:52:22 -0400

    :wrench: Replace excerpt with full content

--- template-parts/content-lccc-event.php
+++ template-parts/content-lccc-event.php
@@ -133,16 +133,15 @@ $cost = event_meta_box_get_meta('event_meta_box_ticket_price_s_');
 		<?php
 			the_content();
 		if(function_exists('lc_addToGoogleCalendar')){
-			$event_excerpt = str_replace('[...]', '', get_the_excerpt());
 		?>
 			<form method="post" action="<?php echo get_stylesheet_directory_uri() ?>/inc/lc-download-ics.php">
 			<input type="hidden" name="date_start" value="<?php echo $eventstartdate . ' ' . $starttime;?>">
 			<input type="hidden" name="date_end" value="<?php echo $endeventdate . ' ' . $endeventtime;?>">
 			<input type="hidden" name="location" value="<?php echo $location;?>">
-			<input type="hidden" name="description" value="<?php echo $event_excerpt;?>">
+			<input type="hidden" name="description" value="<?php echo get_the_content();?>">
 			<input type="hidden" name="summary" value="<?php echo get_the_title();?>">
 			<input type="hidden" name="url" value="<?php echo the_permalink()?>">
-			<a href="<?php echo lc_addToGoogleCalendar(get_the_title(), $eventstartdate . ' ' . $starttime, $endeventdate . ' ' . $endeventtime, $location, $event_excerpt); ?>" target="_blank" class="button add-to-google">Add to my Google Calendar</a> 
+			<a href="<?php echo lc_addToGoogleCalendar(get_the_title(), $eventstartdate . ' ' . $starttime, $endeventdate . ' ' . $endeventtime, $location, get_the_content()); ?>" target="_blank" class="button add-to-google">Add to my Google Calendar</a> 
 			<input type="submit" class="add-to-calendar" value="Add to my Calendar">
 			</form>
 			<?php } ?>
