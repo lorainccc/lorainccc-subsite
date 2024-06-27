f14ab9343082eabd3892351ad683ccac5c50a093
Joe Querin <7998323+joecue@users.noreply.github.com>
2024-06-27 15:15:27 -0400

    :wrench: Update Google Analytics for GA4

--- js/lc-google-analytics.js
+++ js/lc-google-analytics.js
@@ -5,12 +5,10 @@
 
 /** Google Analytics **/
 
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+/**  Google tag (gtag.js) */
 
-	ga('create', 'UA-7542329-2', 'auto', {'allowLinker': true});
-	ga('require', 'linker');
-	ga('linker:autoLink', ['sites.lorainccc.edu'] );
-	ga('send', 'pageview');
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Z27HB3ECDG');
