07a5e9b6c5e5caaebc42f936114c874ebba271b9
Joe Querin <7998323+joecue@users.noreply.github.com>
2024-07-08 12:14:30 -0400

    :wrench: Adding LCCC Announcement Archive Template

---
+++ lc-announce-home.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * Template Name: Announcement HomePage
+ * 
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package LCCC Framework
+ */
+
+get_header();
+
+//Checking to see if this is a home page like MyLCCC
+?>
+<!-- adding spacer -->
+<div class="row">
+	<div class="small-12 medium-12 large-12 columns">&nbsp;</div>
+</div>
+<!-- end spacer row -->
+<div class="row page-content">
+
+	<div class="small-12 medium-8 large-8 columns">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+<?php if ( is_active_sidebar( 'lccc-announcements-sidebar' ) ) { ?>
+						<?php dynamic_sidebar( 'lccc-announcements-sidebar' ); ?>
+				<?php } ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div>
+<div class="small-12 medium-4 large-4 columns">
+	<?php if ( is_active_sidebar( 'lccc-badges-sidebar' ) ) { ?>
+			<div class="small-12 medium-12 large-12 columns hide-for-print">
+			<?php dynamic_sidebar( 'lccc-badges-sidebar' ); ?>
+			</div>
+	<?php } ?>
+			<?php if ( is_active_sidebar( 'lccc-events-sidebar' ) ) { ?>
+																							<?php dynamic_sidebar( 'lccc-events-sidebar' ); ?>
+																		<?php } ?>				
+	</div>
+	<?php
+ //Jetpack Sharing Buttons
+if ( function_exists( 'sharing_display' ) ) {
+    sharing_display( '', true );
+}
+ ?>
+</div>
+
+<?php 
+  
+      get_footer(); ?>
